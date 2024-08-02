534fbdc42171d71e01190de42e00eb7e2ef14f05
Daniel Motz <hello@danielmotz.de>
2024-08-02 17:15:19 +0200

    Initial Commit of Visual Query Builder

---
+++ query-by-graph/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import NetworkVisualiser from "./components/NetworkVisualiser.vue";
+</script>
+
+<template>
+  <div>
+    <NetworkVisualiser></NetworkVisualiser>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
