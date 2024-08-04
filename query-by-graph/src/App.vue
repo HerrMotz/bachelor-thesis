b2ca65b5c2aa2260e9b45af10a0a82ae24e20456
Daniel Motz <hello@daniel-motz.de>
2024-08-04 17:46:33 +0200

    Reformat main layout

--- query-by-graph/src/App.vue
+++ query-by-graph/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
-import { createEditor } from "./editor.ts";
+import {onMounted, ref} from 'vue';
+import {createEditor} from "./editor.ts";
 
 import EntityType from "./types/EntityType.ts";
 
@@ -18,10 +18,10 @@ onMounted(() => {
 
 // mock property numbers array like P160, P141, etc.
 const mockProperties = [
-  { id: "P160", label: "Ausbildende Institution" },
-  { id: "P141", label: "Vater" },
-  { id: "P31", label: "Property 31" },
-  { id: "P279", label: "Property 279" },
+  {id: "P160", label: "Ausbildende Institution"},
+  {id: "P141", label: "Vater"},
+  {id: "P31", label: "Property 31"},
+  {id: "P279", label: "Property 279"},
 ];
 
 const mockIndividuals = [
@@ -43,8 +43,9 @@ interface Editor {
   <div class="h-screen min-h-screen place-items-center bg-white px-6 pb-24 pt-12 sm:pb-32 sm:pt-12 lg:px-8">
     <div class="text-3xl text-center mb-10 font-bold">Query by Graph</div>
     <div class="flex w-full min-h-full bg-amber-100 rounded-2xl" style="height: 100%; min-height: 100%;">
-      <div class="w-4/5 min-h-full p-4 bg-amber-50">
-        <h2 class="text-xl font-semibold mb-4">
+      <div class="w-4/5 min-h-full bg-amber-50">
+        <h2 class="text-xl font-semibold mb-4 bg-amber-100 rounded-tl-2xl p-4">
+          <!-- This has the same propeties as the toolbox heading -->
           Visual Query Builder
           <span class="text-sm ml-2 font-medium">
             Each Box is a SPARQL-Individual and each Connection between them is a SPARQL-Property
@@ -52,79 +53,82 @@ interface Editor {
         </h2>
         <div ref="rete" style="height: calc(100% - 44px);"></div>
       </div>
-      <div v-if="editor" class="w-1/5 overflow-auto flex-col flex gap-6 max-h-full p-4">
-        <h2 class="text-xl font-semibold">
+      <div v-if="editor" class="w-1/5 overflow-auto">
+        <h2 class="text-xl font-semibold bg-amber-200 rounded-tr-2xl p-4">
           Toolbox
           <span class="text-sm ml-2 font-medium">
             Perform actions on the graph
           </span>
         </h2>
-
-        <div class="flex-col flex gap-2">
-          <h4 class="font-semibold">History</h4>
-          <div class="flex gap-4">
-          <Button class="grow" @click="() => { // why the hell is this necessary in TypeScript with Vue3 D':
+        <div class="flex-col flex gap-6 max-h-full p-4">
+          <div class="flex-col flex gap-2">
+            <h4 class="font-semibold">History</h4>
+            <div class="flex gap-4">
+              <Button class="grow" @click="() => { // why the hell is this necessary in TypeScript with Vue3 D':
               if (editor) {
                 editor.then((e: Editor) => e.undo());
               }
             }">
-            Undo
-            <kbd class="inline-flex items-center rounded border border-gray-200 px-1 font-sans text-xs text-gray-200">CTRL+Y</kbd>
-          </Button>
-          <Button class="grow" @click="() => { // why the hell is this necessary in TypeScript with Vue3 D':
+                Undo
+                <kbd
+                    class="inline-flex items-center rounded border border-gray-200 px-1 font-sans text-xs text-gray-200">CTRL+Y</kbd>
+              </Button>
+              <Button class="grow" @click="() => { // why the hell is this necessary in TypeScript with Vue3 D':
               if (editor) {
                 editor.then((e: Editor) => e.redo());
               }
             }">
 
-            Redo
-            <kbd class="inline-flex items-center rounded border border-gray-200 px-1 font-sans text-xs text-gray-200">CTRL+Z</kbd>
-          </Button>
+                Redo
+                <kbd
+                    class="inline-flex items-center rounded border border-gray-200 px-1 font-sans text-xs text-gray-200">CTRL+Z</kbd>
+              </Button>
+            </div>
           </div>
-        </div>
 
-        <div class="flex-col flex gap-2">
-          <h4 class="font-semibold">Create Individual</h4>
-          <EntitySelector :entities="mockIndividuals" @selected-entity="(prop: EntityType) => { // why the hell is this necessary in TypeScript with Vue3 D':
+          <div class="flex-col flex gap-2">
+            <h4 class="font-semibold">Create Individual</h4>
+            <EntitySelector :entities="mockIndividuals" @selected-entity="(prop: EntityType) => { // why the hell is this necessary in TypeScript with Vue3 D':
               if (editor) {
                 editor.then((e: Editor) => e.setSelectedIndividual(prop));
               }
             }" class="bg-amber-300 rounded-2xl p-2">
-            Individual Selector
-          </EntitySelector>
-        </div>
+              Individual Selector
+            </EntitySelector>
+          </div>
 
-        <div class="flex-col flex gap-2">
-          <h4 class="font-semibold">Create connection</h4>
-          <EntitySelector :entities="mockProperties" @selected-entity="(prop: EntityType) => { // why the hell is this necessary in TypeScript with Vue3 D':
+          <div class="flex-col flex gap-2">
+            <h4 class="font-semibold">Create connection</h4>
+            <EntitySelector :entities="mockProperties" @selected-entity="(prop: EntityType) => { // why the hell is this necessary in TypeScript with Vue3 D':
               if (editor) {
                 editor.then((e: Editor) => e.setSelectedProperty(prop));
               }
             }" class="bg-amber-300 rounded-2xl p-2">
-            Property Selector
-          </EntitySelector>
-          <p class="text-gray-600 text-sm hover:text-gray-900 transition-all">
-            <em>Hint:</em>
-            After selecting the appropriate property, add a new connection by clicking
-            an output connector (right side) and then an input connector (left side) of an
-            individual.
-          </p>
-        </div>
-        <div class="flex-col flex gap-2">
-          <h4 class="font-semibold">Delete selected connections</h4>
-          <Button
-              @click="() => { // why the hell is this necessary in TypeScript with Vue3 D':
+              Property Selector
+            </EntitySelector>
+            <p class="text-gray-600 text-sm hover:text-gray-900 transition-all">
+              <em>Hint:</em>
+              After selecting the appropriate property, add a new connection by clicking
+              an output connector (right side) and then an input connector (left side) of an
+              individual.
+            </p>
+          </div>
+          <div class="flex-col flex gap-2">
+            <h4 class="font-semibold">Delete selected connections</h4>
+            <Button
+                @click="() => { // why the hell is this necessary in TypeScript with Vue3 D':
               if (editor) {
                 editor.then((e: Editor) => e.removeSelectedConnection());
               }
             }">
-            Delete selected
-          </Button>
-          <p class="text-gray-600 text-sm hover:text-gray-900 transition-all">
-            <em>Hint:</em>
-            Select a connection by clicking on it and then click the button above to delete it.
-            This only works when at least one connection is selected (red).
-          </p>
+              Delete selected
+            </Button>
+            <p class="text-gray-600 text-sm hover:text-gray-900 transition-all">
+              <em>Hint:</em>
+              Select a connection by clicking on it and then click the button above to delete it.
+              This only works when at least one connection is selected (red).
+            </p>
+          </div>
         </div>
       </div>
     </div>
