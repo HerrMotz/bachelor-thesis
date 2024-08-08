3ad2beea30ec6b136cc1f07c7a298532171e257d
Daniel Motz <daniel.motz@mandant3.de>
2024-08-08 18:36:05 +0200

    Fix timing issue with connection creation, Add clipboard

--- query-by-graph/src/lib.rs
+++ query-by-graph/src/lib.rs
@@ -51,7 +51,7 @@ fn graph_to_query(connections: Vec<Connection>) -> String {
         sparql.push_str(&format!(
             "{}wd:{} wdt:{} wd:{} . # {} -- {} -> {}\n",
             " ".repeat(INDENTATION_COUNT),
-            connection.source.id, connection.property.id, connection.source.id,
+            connection.source.id, connection.property.id, connection.target.id,
             connection.source.label, connection.property.label, connection.target.label
         ));
     }
