0c2d6faa18a00c6e0a6da228aed55af69a5e4c3d
Daniel Motz <hello@danielmotz.de>
2024-12-04 22:09:24 +0100

    Remove path

--- query-by-graph/vite.config.ts
+++ query-by-graph/vite.config.ts
@@ -3,15 +3,9 @@ import
 import vue from '@vitejs/plugin-vue'; 
 import wasm from "vite-plugin-wasm"; 
 import topLevelAwait from "vite-plugin-top-level-await";
-import path from 'path'
 
 // https://vitejs.dev/config/ 
 export default defineConfig(
   {
       plugins: [vue(), wasm(), topLevelAwait()],
-      resolve: {
-          alias: {
-              '@': path.resolve('src')
-          }
-      }
   })
