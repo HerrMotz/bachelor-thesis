5d81a963fc78d65a8264f631371c76e54177011d
Daniel Motz <hello@danielmotz.de>
2024-12-22 00:29:03 +0100

    Add support for the Test Instance data source
    Introduced a new "Test Instance" data source in the constants. Refactored data sources to an array structure and updated logic in `App.vue` to handle dynamic rendering of data source buttons.

--- query-by-graph/src/store.ts
+++ query-by-graph/src/store.ts
@@ -1,10 +1,11 @@
 import { ref } from 'vue';
 import WikibaseDataSource from "./lib/types/WikibaseDataSource.ts";
-import {factGridDataSource, wikiDataDataSource} from "./lib/constants";
+import {factGridDataSource, wikiDataDataSource, testinstanceSource} from "./lib/constants";
 
 
 export const selectedDataSource = ref<WikibaseDataSource>(wikiDataDataSource);
-export const dataSources = {
-        wikidata: wikiDataDataSource,
-        factgrid: factGridDataSource,
-};
+export const dataSources = [
+        wikiDataDataSource,
+        factGridDataSource,
+        testinstanceSource
+];
